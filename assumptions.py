9a1a4aaaaf7f6645039131a2884b9286c47d670f
Mathias <mathiasmollersorensen93@gmail.com>
2021-09-20 19:13:18 +0200

    Round6

--- assumptions.py
+++ assumptions.py
@@ -1,11 +1,11 @@
 from datetime import datetime, timedelta
 date = datetime.today().date()
-current_round = 5
+current_round = 6
 forecast_window = 6
 discount_factor = 0.5
-sub_1_discount = 0.5
+sub_1_discount = 0.2
 sub_2_discount = 0.1
-sub_3_discount = 0.1
+sub_3_discount = 0.05
 sub_gk_discount = 0.01
 cash = 0
 n_transfer = 1
